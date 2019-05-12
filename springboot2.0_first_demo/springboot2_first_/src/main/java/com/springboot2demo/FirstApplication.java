b6dd7d80e3fce77f021e180abadd33182f7ead2d
cslj2013 <1136958892@qq.com>
2019-05-12 12:59:24 +0800

    @componentscan指定扫描范围

--- springboot2.0_first_demo/springboot2_first_/src/main/java/com/springboot2demo/FirstApplication.java
+++ springboot2.0_first_demo/springboot2_first_/src/main/java/com/springboot2demo/FirstApplication.java
@@ -1,20 +1,17 @@
 package com.springboot2demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@SpringBootApplication //或使用@EnableAutoConfiguration配置
-//@EnableAutoConfiguration //自动配置,根据pom文件引入的依赖信息,自动配置对应的组件;扫包范围，默认在当前类中
+//@RestController
+//@SpringBootApplication //或使用@EnableAutoConfiguration配置
+@EnableAutoConfiguration //自动配置,根据pom文件引入的依赖信息,自动配置对应的组件;扫包范围，默认在当前类中
+@ComponentScan("com.springboot2demo") //指定扫描范围
 public class FirstApplication {
 
-	@RequestMapping("/test")
-	public String test(){
-		return "springboot2.0 first application";
-	}
-	
 	/**
 	 * 程序入口
 	 *  SpringApplication.run 相当于java代码创建内置tomcat，加载springmvc注解启动
